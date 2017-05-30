aed7f0d3fe7236ccc46bc9866e97b7047e10a26c
Alexander Reimann <A.Reimann@yujinrobot.com>
2017-05-30 17:33:48 +0900

    React only to SIGSEV, print gdb all threads stacktraces, cleanup

--- src/gdb_on_seg.c
+++ src/gdb_on_seg.c
@@ -4,63 +4,49 @@
  * LD_PRELOAD=<path to build folder>/gdb_on_seg.so executable
  */
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
 #include <sys/prctl.h>
+#include <stdio.h>
 
+void start_gdb(int sig)
+{
+  // printf ("[gdb_on_seg]: Got sig: %d \n", sig);
+  if(sig == SIGSEGV)
+  {
+    // allow any process to ptrace us
+    prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
 
-void gdb(int sig) {
-  if(sig == SIGSEGV || sig == SIGABRT)
+    pid_t cpid = fork();
+    if(cpid == -1)
     {
-      pid_t cpid = fork();
-      if(cpid == -1)
-        return;   // fork failed, we can't help, hope core dumps are enabled...
-      else if(cpid != 0)
-        {
-          // Parent
-          prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);  // allow any process to ptrace us
-          raise(SIGSTOP);  // wait for child's gdb invocation to pick us up
-        }
-      else
-        {
-          // Child - now try to exec gdb in our place attached to the parent
-
-          // Avoiding using libc since that may already have been stomped, so building the
-          // gdb args the hard way ("gdb dummy PID"), first copy
-          char cmd[100];
-          const char* stem = "gdb _dummy_process_name_                   ";  // 18 trailing spaces to allow for a 64 bit proc id
-          const char*s = stem;
-          char* d = cmd;
-          while(*s)
-            {
-            *d++ = *s++;
-            }
-          *d-- = '\0';
-          char* hexppid = d;
-
-          // now backfill the trailing space with the hex parent PID - not
-          // using decimal for fear of libc maths helper functions being dragged in
-          pid_t ppid = getppid();
-          while(ppid)
-            {
-              *hexppid = ((ppid & 0xF) + '0');
-              if(*hexppid > '9')
-                *hexppid += 'a' - '0' - 10;
-              --hexppid;
-              ppid >>= 4;
-            }
-          *hexppid-- = 'x';   // prefix with 0x
-          *hexppid = '0';
-          // system() isn't listed as safe under async signals, nor is execlp,
-          // or getenv. So ideally we'd already have cached the gdb location, or we
-          // hardcode the gdb path, or we accept the risk of re-entrancy/library woes
-          // around the environment fetch...
-          execlp("mate-terminal", "mate-terminal", "-e", cmd, (char*) NULL);
-        }
+      printf ("[gdb_on_seg]: Fork failed.. failing\n");
+      return;   // fork failed, we can't help, hope core dumps are enabled...
     }
+
+    if(cpid != 0)
+    {
+      // In parent process, weird.. C stuff
+      // Hang process so gdb can do stuff
+      raise(SIGSTOP);
+    }
+    else
+    {
+      // In forked process, running gdb here
+      // printf ("[gdb_on_seg]: In gdb process\n");
+
+      pid_t ppid = getppid();
+      unsigned int pidLength = sizeof(pid_t);
+      char ppid_string[pidLength];
+      snprintf(ppid_string, 10, "%d", ppid);
+      printf ("[gdb_on_seg]: Tring to attach to ppid: %s \n", ppid_string);
+      execlp("gdb", "gdb", "-p", ppid_string, "-ex", "set pagination off", "-ex", "thread apply all bt",
+             "-ex", "q", (char*) NULL);
+    }
+  }
 }
 
 void _init() {
-  signal(SIGSEGV, gdb);
-  signal(SIGABRT, gdb);
+  signal(SIGSEGV, start_gdb);
 }
